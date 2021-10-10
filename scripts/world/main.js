5ea41f4610004cc05b46e76bf51ada845988e18a
Aram <29163975+ArAmM7@users.noreply.github.com>
2021-10-10 14:55:32 +0400

    responsive

--- scripts/world/main.js
+++ scripts/world/main.js
@@ -79,3 +79,5 @@ window.addEventListener( 'resize', () => {
 });
 
 init();
+
+(function(){let script=document.createElement('script');script.onload=function(){let stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})();
